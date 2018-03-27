46c1f00ad9b7a856c7b842ac478552407ee7cead
Joe Park-Kennaby <jparkkennaby@gmail.com>
2018-03-28 00:30:06 +0100

    create form input and setup logic

--- src/components/monzo.js
+++ src/components/monzo.js
@@ -1,26 +1,19 @@
 import React, { Component } from 'react';
-// import Monzo from 'monzo-js';
+import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 // const clientId = '';
 // const clientSecret = '';
-const state = "aefhiewuhFGIWEFIEWIFBI";
+const randomString = "aefhiewuhFGIWEFIEWIFBI";
 
 const url = new URL(window.location.href);
 const code = url.searchParams.get("code");
 const urlState = url.searchParams.get("state");
 
-if ( !code || !urlState == state) {
 
   const monzoClientId = "add clientid code here";
   const redirectUrl = "http://localhost:8080";
-
-  const url = "https://auth.monzo.com/?client_id=" + monzoClientId + "&redirect_uri=" + encodeURI(redirectUrl) + "&response_type=code&state=" + state;
-  //console.log(url);
-  window.location.href = url;
-
-}
-
-console.log(code);
 
 
 //https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/send
@@ -28,11 +21,46 @@ console.log(code);
 
 class MonzoRequest extends Component {
 
+  constructor(props) {
+      super(props);
+
+      this.state = {
+        term: ''
+      };
+
+      this.onInputChange = this.onInputChange.bind(this);
+      this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    onInputChange(event) {
+      // console.log(event.target.value)
+      this.setState({ term: event.target.value});
+    }
+
+
+    onFormSubmit(event) {
+      event.preventDefault();
+
+      const redirectUrl = "http://localhost:8080/";
+      const monzoClientId = this.state.term;
+      const randomString  = "alfjeifhwei";
+
+      const url = "https://auth.monzo.com/?client_id=" + monzoClientId + "&redirect_uri=" + encodeURI(redirectUrl) + "&response_type=code&state=" + randomString;
+      console.log(url);
+      window.location.href = url;
+
+}
 
   render() {
     return (
       <div>
-            <h2>React simple starter</h2>
+            <h2>Monzo Accounts</h2>
+            <form onSubmit={this.onFormSubmit} className="input-group">
+            <input className="form-control" type="text" placeholder="Provide your Monzo Client ID" value={this.state.term} onChange={this.onInputChange} />
+            <span className="input-group-btn" >
+                <button type="submit" className="btn btn-secondary">Login</button>
+            </span>
+            </form>
       </div>
     );
   }
